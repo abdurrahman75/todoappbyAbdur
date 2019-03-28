cd347e4d6ba19f932a2c8f18cc7683d5574e5747
abdurrahman75 <abdur.rahman@email.com>
2019-03-28 16:48:00 -0400

    Update keys.js

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://todoapp:bangladesh71@ds253918.mlab.com:53918/todoapp",
+  mongoURI: "mongodb://@ds253918.mlab.com:53918/todoapp",
   secretOrKey: "secret"
 };
