87535ffc95f024e71b777ac94edcfe186a5716e1
abdurrahman75 <abdur.rahman@email.com>
2020-03-30 23:47:58 -0400

    Update todo-list.js

--- client/src/components/todo-list.js
+++ client/src/components/todo-list.js
@@ -58,7 +58,7 @@ class TodosList extends Component {
     }
 
     componentDidMount() {
-        axios.get('http://localhost:4000/todos/')
+        axios.get('https://localhost:4000/todos/')
             .then(response => {
                 this.setState({ todos: response.data });
             })
